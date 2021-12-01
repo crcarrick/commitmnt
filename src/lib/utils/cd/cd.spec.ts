d9a1769ed71de9cd483d188bf6d4e75a3784a7ab
Chris Carrick <christophercarrick7@gmail.com>
2021-12-01 15:01:33 -0500

    feat: add `ChangeDirectory` utility class
    the `ChangeDirectory` utility class provides a way to track the processes
    movements around the filesystem. it also provides a way to assert
    that the process is in an expected directory.
    in a future  version, we will use this utility to move around the filesystem and
    verify that the process is in the directory we expect before taking certain
    actions

--- src/lib/utils/cd/cd.spec.ts
+++ src/lib/utils/cd/cd.spec.ts
@@ -1,1 +1,57 @@
-it.todo('test the cd utility');
+import { ChangeDirectory } from './cd';
+
+const baseDir = '~/';
+const projectsDir = '~/Projects';
+
+let cd: ChangeDirectory;
+
+function mockProcess() {
+  let current: string;
+
+  jest.spyOn(process, 'chdir').mockImplementation((dir) => (current = dir));
+  jest.spyOn(process, 'cwd').mockImplementation(() => current);
+}
+
+beforeEach(() => {
+  mockProcess();
+
+  cd = new ChangeDirectory(baseDir);
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
+it('changes to the base directory when constructed', () => {
+  expect(process.chdir).toHaveBeenCalledWith(baseDir);
+  expect(cd.current).toBe(baseDir);
+});
+
+it('changes to a provided directory', () => {
+  cd.go(projectsDir);
+
+  expect(process.chdir).toHaveBeenCalledWith(projectsDir);
+  expect(cd.current).toBe(projectsDir);
+});
+
+it('saves the previous directory when changing directories', () => {
+  cd.go(projectsDir);
+
+  expect(cd.last).toBe(baseDir);
+});
+
+it('goes back to the previous directory', () => {
+  cd.go(projectsDir);
+  cd.back();
+
+  expect(process.chdir).toHaveBeenCalledWith(baseDir);
+  expect(cd.current).toBe(baseDir);
+});
+
+it('ensures the current directory is the provided directory', () => {
+  expect(cd.ensure(baseDir)).toBe(true);
+});
+
+it('throws an error when the current directory is not the provided directory', () => {
+  expect(() => cd.ensure(projectsDir)).toThrowError(/Unexpected directory/);
+});
--- src/lib/utils/cd/cd.ts
+++ src/lib/utils/cd/cd.ts
@@ -1,4 +1,49 @@
-// TODO: Need to come up with some central location to handle directory changes from
-//       with some safeguards in place.  Have encountered a bunch of issues while testing
-//       the lib that stemmed from exec commands being executed from the wrong directory
-//       and the codebase is littered with process.chdirs in kind of random places.
+export class ChangeDirectory {
+  current: string;
+  last: string;
+
+  /**
+   *
+   * @param base the directory to start at
+   * @constructor
+   */
+  constructor(baseDir: string) {
+    this.go(baseDir);
+    this.ensure(baseDir);
+  }
+
+  /**
+   * Changes back to the last directory
+   */
+  back() {
+    const last = this.last;
+
+    this.go(last);
+  }
+
+  /**
+   * Changes to a directory
+   *
+   * @param dir the directory to go to
+   */
+  go(dir: string) {
+    this.last = process.cwd();
+    process.chdir(dir);
+    this.current = process.cwd();
+    this.ensure(dir);
+  }
+
+  /**
+   * Ensures the process is in the specified directory
+   *
+   * @param dir the directory to test
+   * @returns true if the directory matches
+   * @throws error if the directory does not match
+   */
+  ensure(dir: string) {
+    if (process.cwd() !== dir)
+      throw new Error(`Unexpected directory: ${process.cwd()} - Expected: ${dir}`);
+
+    return true;
+  }
+}
